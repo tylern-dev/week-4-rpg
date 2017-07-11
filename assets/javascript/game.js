95281055bdf44778741141c6816c7dc34486ec63
Tyler Negro <tnegro@gmail.com>
2017-07-10 21:59:13 -0600

    added working files

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// move content from one div to another $('#div1').contents().appendTo('#div2')
+//pseudo:
+// when the game starts display characters with their HP
+
+// when a character is selected, that is the players choice
+// 	other characters need to move to enemies available to attack stage
+// select the character to fight
+// 	move that character to defender stage
+
+// when attack is pressed
+// 	player attack power increases by base attack power (3,6,9,12,...)
+// 	enemy hp - attack power
+// 	enemy attackpower decreases player hp
+
+
