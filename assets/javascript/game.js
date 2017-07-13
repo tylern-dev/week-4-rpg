e32ecd6c314a0933121a5537ca9c25361c8e8b03
Tyler Negro <tnegro@gmail.com>
2017-07-12 22:29:20 -0600

    characters move to right location, but buttons aren't working.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,4 +11,70 @@
 // 	player attack power increases by base attack power (3,6,9,12,...)
 // 	enemy hp - attack power
 // 	enemy attackpower decreases player hp
+var playerCharacter;
+var charId;
 
+var characters = [
+    {
+        name:'Boba Fett',
+        id:'boba',
+        hp: hpGenerator(),
+        ap:'',
+        cap:'', 
+    },
+    {
+        name:'Luke',
+        id:'luke',
+        hp: hpGenerator(),
+        ap:'',
+        cap:'',
+    },
+    {
+        name:'Leia',
+        id:'leia',
+        hp: hpGenerator(),
+        ap:'',
+        cap:'',
+    },
+    {
+        name:'Vader',
+        id:'vader',
+        hp: hpGenerator(),
+        ap:'',
+        cap:'',
+    }
+]
+
+
+
+
+
+function hpGenerator(){
+    return 50+50*Math.floor(Math.random()*5);
+}
+
+$('.character-button').on('click', function(event){
+    charId = event.currentTarget.id;
+    characterSelect(charId);
+
+});
+
+//detects which character was pressed and then moves the characters to the appropriate section and adjusts classes
+function characterSelect(char){
+    for(var i = 0; i<characters.length; i++){
+        if(char === characters[i].id){
+            $('#'+characters[i].id).contents().appendTo('.character-select');
+            $('.character-button').addClass("hidden");
+            $('.character-select').removeClass("hidden");
+        } 
+        else if(char != characters[i].id){
+            // var test = $('#'+characters[i].id).append('<button>')
+            $('.enemy-section').append('<button id='+characters[i].id+'>')
+            $('#'+characters[i].id).contents().appendTo('.enemy-section');
+            
+            $('.enemy-section').removeClass("hidden");
+            // console.log('players not selected: '+ characters[i].id)
+        }
+    }
+}
+    
